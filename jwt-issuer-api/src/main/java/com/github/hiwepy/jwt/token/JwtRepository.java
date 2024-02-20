d97d1f999b1f4d363f26b9061345c8c95a096820
wandl <hiwepy@163.com>
2024-02-20 11:08:42 +0800

    1.1.2-SNAPSHOT

--- jwt-issuer-api/src/main/java/com/github/hiwepy/jwt/token/JwtRepository.java
+++ jwt-issuer-api/src/main/java/com/github/hiwepy/jwt/token/JwtRepository.java
@@ -6,15 +6,58 @@ import com.github.hiwepy.jwt.JwtPayload;
 import com.github.hiwepy.jwt.exception.JwtException;
 
 public interface JwtRepository<S>{
-	
-	public abstract String issueJwt(S signingKey, String jwtId, String subject, String issuer, String audience,
+
+	/**
+	 * 生成JWT令牌
+	 * @param signingKey 签名密钥
+	 * @param jwtId 令牌ID
+	 * @param subject 主题
+	 * @param issuer 签发者
+	 * @param audience 接收者
+	 * @param roles 角色
+	 * @param permissions 权限
+	 * @param algorithm 算法
+	 * @param period 有效期
+	 * @return JWT令牌
+	 * @throws JwtException Jwt异常
+	 */
+	String issueJwt(S signingKey, String jwtId, String subject, String issuer, String audience,
 			String roles, String permissions, String algorithm, long period) throws JwtException;
 
-	public abstract String issueJwt(S signingKey, String jwtId, String subject, String issuer, String audience,
+	/**
+	 * 生成JWT令牌
+	 * @param signingKey 签名密钥
+	 * @param jwtId 令牌ID
+	 * @param subject 主题
+	 * @param issuer 签发者
+	 * @param audience 接收者
+	 * @param claims 声明
+	 * @param algorithm 算法
+	 * @param period 有效期
+	 * @return JWT令牌
+	 * @throws JwtException Jwt异常
+	 */
+	String issueJwt(S signingKey, String jwtId, String subject, String issuer, String audience,
 			Map<String, Object> claims, String algorithm, long period) throws JwtException;
-	
-	public abstract boolean verify(S signingKey, String token, boolean checkExpiry) throws JwtException;
-	
-	public abstract JwtPayload getPlayload(S signingKey, String token, boolean checkExpiry) throws JwtException;
-	
+
+	/**
+	 * 验证JWT令牌
+	 * @param signingKey 签名密钥
+	 * @param token 令牌
+	 * @param checkExpiry 是否检查过期
+	 * @return 是否验证通过
+	 * @throws JwtException Jwt异常
+	 */
+	boolean verify(S signingKey, String token, boolean checkExpiry) throws JwtException;
+
+	/**
+	 * 获取JWT内容
+	 * @param signingKey 签名密钥
+	 * @param token 令牌
+	 * @param checkExpiry 是否检查过期
+	 * @return JWT内容
+	 * @throws JwtException Jwt异常
+	 */
+	JwtPayload getPlayload(S signingKey, String token, boolean checkExpiry) throws JwtException;
+
 }
