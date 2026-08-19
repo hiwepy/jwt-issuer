7239255d31f3ba6ccd5947a9e465b490aead45e8
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-19 17:54:08 +0800

    build(pom): normalize pom.xml formatting (4-space indent)

---
+++ jwt-issuer-api/src/main/java/io/github/easy4j/jwt/exception/IncorrectJwtException.java
@@ -0,0 +1,27 @@
+package io.github.easy4j.jwt.exception;
+
+@SuppressWarnings("serial")
+/**
+ * JWT implementation class.
+ *
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
+ */
+public class IncorrectJwtException extends JwtException {
+	
+	public IncorrectJwtException() {
+		super();
+	}
+
+	public IncorrectJwtException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public IncorrectJwtException(String message) {
+		super(message);
+	}
+
+	public IncorrectJwtException(Throwable cause) {
+		super(cause);
+	}
+	
+}
